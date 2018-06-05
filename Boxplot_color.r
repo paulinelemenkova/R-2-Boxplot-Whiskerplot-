ad276e2d820fea7cabc1694bf55f5143dd7d49a9
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2018-06-05 09:19:51 +0800

    Add files via upload

--- Boxplot_color.r
+++ Boxplot_color.r
@@ -1,21 +1,24 @@
 #BoxPlot (or WhiskerPlot)
-
+# ЧАСТЬ 1: делаем data.frame
+	# шаг-1. вчитываем таблицу. делаем из нее датафрейм.
 MDepths <- read.csv("Depths.csv", header=TRUE, sep = ",")
-MDepth<- data.frame(MDepths, nrow = 1008, ncol = 25)	
-MDepthsBoxplot<- boxplot(
-	MDepths$profile1, MDepths$profile2, MDepths$profile3, MDepths$profile4, MDepths$profile5, MDepths$profile6, 
-	MDepths$profile7, MDepths$profile8, MDepths$profile9, MDepths$profile10, MDepths$profile11, 
-	MDepths$profile12, MDepths$profile13, MDepths$profile14, MDepths$profile15, MDepths$profile16, 
-	MDepths$profile17, MDepths$profile18, MDepths$profile19, MDepths$profile20, MDepths$profile21, 
-	MDepths$profile22, MDepths$profile23, MDepths$profile24, MDepths$profile25,     
+	# шаг-2. чистим датафрейм от NA значений
+MDepths <- na.omit(MDepths) 
+row.has.na <- apply(MDF, 1, function(x){any(is.na(x))}) # проверяем, удалил ли все NA
+sum(row.has.na) # суммируем все NA, должно получиться: [1] 0
+head(MDepths) # смотрим очищенный датафрейм. теперь с ним работаем.
+
+# ЧАСТЬ 2: делаем диаграмму распределения данных "whisker boxplot"
+	# шаг-3. создаем лист названий профилей	(что будет отображаться по оси X)
+profile_names <- paste(c("profile"), seq(1:25), sep="") 
+	# шаг-4. строим боксплот по указанным аргументам (здесь: 25 профилей)
+p<- boxplot(MDepths$profile1, MDepths$profile2, MDepths$profile3, MDepths$profile4, MDepths$profile5, MDepths$profile6,MDepths$profile7, MDepths$profile8, MDepths$profile9, MDepths$profile10, MDepths$profile11, MDepths$profile12, MDepths$profile13, MDepths$profile14, MDepths$profile15, MDepths$profile16, MDepths$profile17, MDepths$profile18, MDepths$profile19, MDepths$profile20, MDepths$profile21, MDepths$profile22, MDepths$profile23, MDepths$profile24, MDepths$profile25,     
 	main = "Mariana Trench Depths Boxplot", 
 	outline = TRUE,
 	outlier.color = "seagreen", outlier.shape = 8, outlier.size = 2,    
-#	xlab = "Nr. of profiles",         
+#	xlab = "Profiles",         
 	ylab = "Depths",         
 	las = 2,         
 	col = viridis(25, alpha=.2),         
-	names = c("profile1", "profile2", "profile3", "profile4", "profile5", "profile6", "profile7",
-"profile8", "profile9", "profile10","profile11", "profile12", "profile13", "profile14", "profile15",
-"profile16", "profile17", "profile18", "profile19", "profile20","profile21", "profile22", "profile23",
-"profile24", "profile25"))
+	names = profile_names)
+p
